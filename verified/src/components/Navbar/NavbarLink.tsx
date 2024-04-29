37ebdae7f9ca8946f83321311e088685179f140d
Martin Tvarog <martin.tvarog@gmail.com>
2024-04-30 00:07:46 +0200

    redesign the header

--- verified/src/components/Navbar/NavbarLink.tsx
+++ verified/src/components/Navbar/NavbarLink.tsx
@@ -9,18 +9,19 @@ import { Route } from "next";
 
 type Props = PropsWithChildren<{
   to: Route;
+  className?: string;
 }>;
 
-export const NavbarLink = ({ to, children }: Props) => {
+export const NavbarLink = ({ to, children, className }: Props) => {
   const pathname = usePathname();
   const isActive = pathname.startsWith(to);
 
   return (
-    <Link href={to}>
+    <Link href={to} className={className}>
       <Button
         variant="link"
         className={cn(
-          "font-semibold",
+          "font-semibold w-full",
           isActive && "!bg-[#646cff] text-white"
         )}
       >
--- verified/src/components/Navbar/index.tsx
+++ verified/src/components/Navbar/index.tsx
@@ -3,16 +3,43 @@
 import React from "react";
 import { AppBar, Toolbar } from "@mui/material";
 import { NavbarLink } from "./NavbarLink";
+import { Sixtyfour } from "next/font/google";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+
+const sixtyFour = Sixtyfour({ subsets: ["latin"] });
 
 const NavBar: React.FC = () => (
-  <AppBar position="sticky" style={{ backgroundColor: "#242424" }}>
-    <Toolbar
-      className="flex justify-center space-x-4"
+  <AppBar
+    position="sticky"
+    style={{ backgroundColor: "#242424" }}  
+  >
+    <div 
+      className={cn(
+        "flex flex-col p-4 max-w-7xl mx-auto w-full space-y-4",
+        "lg:grid lg:grid-cols-[repeat(2,auto)] lg:space-y-0 lg:space-x-4 lg:items-center"
+      )}
     >
-      <NavbarLink to="/createAttestation">Create Attestation</NavbarLink>
-      <NavbarLink to="/viewAttestation">View Attestation</NavbarLink>
-      <NavbarLink to="/verifyAttestation">Verify Attestation</NavbarLink>
-    </Toolbar>
+      <Link href="/">
+        <h1 className={cn(
+          "text-blue-400 text-3xl text-center",
+          "lg:pl-4 lg:text-left",
+          sixtyFour.className
+        )}>
+          Verified
+        </h1>
+      </Link>
+      <Toolbar
+        className={cn(
+          "flex flex-col space-y-4 justify-end",
+          "sm:flex-row sm:space-x-4 sm:space-y-0 sm:self-center"
+        )}
+      >
+        <NavbarLink className="sm:w-auto w-full" to="/createAttestation">Create Attestation</NavbarLink>
+        <NavbarLink className="sm:w-auto w-full" to="/viewAttestation">View Attestation</NavbarLink>
+        <NavbarLink className="sm:w-auto w-full" to="/verifyAttestation">Verify Attestation</NavbarLink>
+      </Toolbar>
+    </div>
   </AppBar>
 );
 export default NavBar;
