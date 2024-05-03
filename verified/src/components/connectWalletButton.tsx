8899855e86df0de396fc7bdda8e9f0ddc5ad35e9
Martin Tvarog <martin.tvarog@gmail.com>
2024-05-03 23:14:51 +0200

    refactor connectWalletButton.tsx

--- verified/src/components/connectWalletButton.tsx
+++ verified/src/components/connectWalletButton.tsx
@@ -1,23 +1,24 @@
-// import WalletService from "../services/walletService.ts";
-// import {Redirect, useHistory} from "react-router-dom";
-//
-// const ConnectWalletButton = () => {
-//
-//     const history = useHistory();
-//
-//     const connectWallet = async () => {
-//         const connected = await WalletService.connectWallet();
-//         if (connected) {
-//             return <Redirect to={'/createAttestation'}/>
-//         }
-//     };
-//
-//     return (
-//         <div>
-//             <button onClick={connectWallet}>Connect Wallet</button>
-//         </div>
-//
-//     );
-// };
-//
-// export default ConnectWalletButton;
+import WalletService from "@/services/walletService";
+import {useRouter} from "next/navigation";
+
+
+const ConnectWalletButton = () => {
+
+   const router = useRouter();
+
+    const connectWallet = async () => {
+        const connected = await WalletService.connectWallet();
+        if (connected) {
+            router.push('/createAttestation');
+        }
+    };
+
+    return (
+        <div>
+            <button onClick={connectWallet}>Connect Wallet</button>
+        </div>
+
+    );
+};
+
+export default ConnectWalletButton;
