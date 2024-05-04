a0c0651b3f0e7eb3e974daded93ce342d2ebaac5
Martin Tvarog <martin.tvarog@gmail.com>
2024-05-04 18:11:19 +0200

    remove unneccesary logging

--- verified/src/services/walletService.ts
+++ verified/src/services/walletService.ts
@@ -1,13 +1,10 @@
 const connectWallet = async (): Promise<boolean> => {
     try {
         if (window.ethereum) {
-            // Request account access if needed
             await window.ethereum.request({method: 'eth_requestAccounts'});
             console.log('Wallet connected successfully!');
-            console.log('Wallet address:', await window.ethereum.request({method: 'eth_accounts'}));
             return true;
-            // You can add more logic here after the wallet is connected
-        } else {
+        } else if(window.ethereum === undefined) {
             console.log('Please install Metamask to connect your wallet.');
             return false;
         }
