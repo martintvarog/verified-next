dee984ab50f1003d252056e7c533701de139d9b3
Martin Tvarog <martin.tvarog@gmail.com>
2024-05-05 19:49:33 +0200

    fix tests

--- verified/tests/components/inputFile.test.tsx
+++ verified/tests/components/inputFile.test.tsx
@@ -1,43 +1,33 @@
 // unittest for inputFile.tsx
 
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import { InputFile } from '@/components/inputFile';
-import { fileToByteArray, createHash } from '@/utils/fileHelpers';
+import {render, screen} from '@testing-library/react';
+import {InputFile} from '@/components/inputFile';
+import {fileToByteArray, createHash} from '@/utils/fileHelpers';
 import {userEvent} from "@testing-library/user-event";
 
-jest.mock('@/utils/fileHelpers', () => ({
-  fileToByteArray: jest.fn(),
-  createHash: jest.fn(),
+jest.mock('verified/src/utils/fileHelpers', () => ({
+    fileToByteArray: jest.fn(),
+    createHash: jest.fn(),
 }));
 
 describe('InputFile', () => {
-  it('should display an error alert when the file type is not a PDF', async () => {
-    render(<InputFile onChange={jest.fn()} />);
-    const input = screen.getByLabelText('Document');
-    const file = new File([''], 'file.txt', { type: 'text/plain' });
+    it('should call onChange with the hash of the file', async () => {
+        const onChange = jest.fn();
+        const hash = 'hash';
+        const byteArray = new Uint8Array([0, 1, 2, 3]);
 
-    await userEvent.upload(input, file);
+        (fileToByteArray as jest.Mock).mockResolvedValue(byteArray);
+        (createHash as jest.Mock).mockResolvedValue(hash);
 
-    expect(screen.getByText('Invalid file type. Please upload a PDF file'));
-  });
+        render(<InputFile onChange={onChange}/>);
+        const input = screen.getByLabelText('Document');
+        const file = new File([''], 'file.pdf', {type: 'application/pdf'});
 
-  it('should call onChange with the hash of the file', async () => {
-    const onChange = jest.fn();
-    const hash = 'hash';
-    const byteArray = new Uint8Array([0, 1, 2, 3]);
+        await userEvent.upload(input, file);
 
-    (fileToByteArray as jest.Mock).mockResolvedValue(byteArray);
-    (createHash as jest.Mock).mockResolvedValue(hash);
-
-    render(<InputFile onChange={onChange} />);
-    const input = screen.getByLabelText('Document');
-    const file = new File([''], 'file.pdf', { type: 'application/pdf' });
-
-    await userEvent.upload(input, file);
-
-    expect(fileToByteArray).toHaveBeenCalledWith(file);
-    expect(createHash).toHaveBeenCalledWith(byteArray);
-    expect(onChange).toHaveBeenCalledWith(hash);
-  });
+        expect(fileToByteArray).toHaveBeenCalledWith(file);
+        expect(createHash).toHaveBeenCalledWith(byteArray);
+        expect(onChange).toHaveBeenCalledWith(hash);
+    });
 });
