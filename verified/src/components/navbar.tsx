2375749335c6d1c48c90681fecd6379770ac5ca6
Martin Tvarog <martin.tvarog@gmail.com>
2024-05-07 19:02:09 +0200

    add logo

--- verified/src/components/navbar.tsx
+++ verified/src/components/navbar.tsx
@@ -2,10 +2,32 @@ import Link from 'next/link';
 import React from 'react';
 import { AppBar, Toolbar } from '@mui/material';
 import {Button} from "@/components/ui/button";
+import {cn} from "@/lib/utils";
+import { Sixtyfour } from "next/font/google";
+
+const logoFont = Sixtyfour({ subsets: ["latin"] });
 
 const NavBar: React.FC = () => {
     return (
-        <AppBar position="static"  style={{ backgroundColor: '#242424' }} >
+        <AppBar
+            position="sticky"
+            style={{ backgroundColor: "#242424" }}
+        >
+            <div
+                className={cn(
+                    "flex flex-col p-4 max-w-7xl mx-auto w-full space-y-4",
+                    "lg:grid lg:grid-cols-[repeat(2,auto)] lg:space-y-0 lg:space-x-4 lg:items-center"
+                )}
+            >
+            <Link href="/">
+                <h1 className={cn(
+                    "text-blue-400 text-3xl text-center",
+                    "lg:pl-4 lg:text-left",
+                    logoFont.className
+                )}>
+                    Verified
+                </h1>
+            </Link>
             <Toolbar className="justify-center items-center flex">
                 <Link href={"createAttestation"}>
                     <Button className="mr-4">
@@ -23,6 +45,7 @@ const NavBar: React.FC = () => {
                     </Button>
                 </Link>
             </Toolbar>
+            </div>
         </AppBar>
     );
 };
