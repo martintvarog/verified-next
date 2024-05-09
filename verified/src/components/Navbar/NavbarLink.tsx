009f9f0c2bca1b93704c8652241154a21c2593ae
Martin Tvarog <martin.tvarog@gmail.com>
2024-05-10 00:48:52 +0200

    format

--- verified/src/components/Navbar/NavbarLink.tsx
+++ verified/src/components/Navbar/NavbarLink.tsx
@@ -1,32 +1,32 @@
 "use client";
 
-import { cn } from "@/lib/utils";
+import {cn} from "@/lib/utils";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
-import { PropsWithChildren } from "react";
-import { Button } from "@/components/ui/button";
-import { Route } from "next";
+import {usePathname} from "next/navigation";
+import {PropsWithChildren} from "react";
+import {Button} from "@/components/ui/button";
+import {Route} from "next";
 
 type Props = PropsWithChildren<{
-  to: Route;
-  className?: string;
+    to: Route;
+    className?: string;
 }>;
 
-export const NavbarLink = ({ to, children, className }: Props) => {
-  const pathname = usePathname();
-  const isActive = pathname.startsWith(to);
+export const NavbarLink = ({to, children, className}: Props) => {
+    const pathname = usePathname();
+    const isActive = pathname.startsWith(to);
 
-  return (
-    <Link href={to} className={className}>
-      <Button
-        variant="link"
-        className={cn(
-          "font-semibold w-full",
-          isActive && "!bg-[#646cff] text-white"
-        )}
-      >
-        {children}
-      </Button>
-    </Link>
-  );
+    return (
+        <Link href={to} className={className}>
+            <Button
+                variant="link"
+                className={cn(
+                    "font-semibold w-full",
+                    isActive && "!bg-[#646cff] text-white"
+                )}
+            >
+                {children}
+            </Button>
+        </Link>
+    );
 };
--- verified/src/components/Navbar/index.tsx
+++ verified/src/components/Navbar/index.tsx
@@ -1,44 +1,44 @@
 "use client";
 
-import { AppBar, Toolbar } from "@mui/material";
-import { NavbarLink } from "./NavbarLink";
-import { Sixtyfour } from "next/font/google";
-import { cn } from "@/lib/utils";
+import {AppBar, Toolbar} from "@mui/material";
+import {NavbarLink} from "./NavbarLink";
+import {Sixtyfour} from "next/font/google";
+import {cn} from "@/lib/utils";
 import Link from "next/link";
 
-const logoFont = Sixtyfour({ subsets: ["latin"] });
+const logoFont = Sixtyfour({subsets: ["latin"]});
 
 const NavBar: React.FC = () => (
-  <AppBar
-    position="sticky"
-    style={{ backgroundColor: "#242424" }}  
-  >
-    <div 
-      className={cn(
-        "flex flex-col p-4 max-w-7xl mx-auto w-full space-y-4",
-        "lg:grid lg:grid-cols-[repeat(2,auto)] lg:space-y-0 lg:space-x-4 lg:items-center"
-      )}
+    <AppBar
+        position="sticky"
+        style={{backgroundColor: "#242424"}}
     >
-      <Link href="/">
-        <h1 className={cn(
-          "text-blue-400 text-3xl text-center",
-          "lg:pl-4 lg:text-left",
-          logoFont.className
-        )}>
-          Verified
-        </h1>
-      </Link>
-      <Toolbar
-        className={cn(
-          "flex flex-col space-y-4 justify-end",
-          "sm:flex-row sm:space-x-4 sm:space-y-0 sm:self-center"
-        )}
-      >
-        <NavbarLink className="sm:w-auto w-full" to="/createAttestation">Create Attestation</NavbarLink>
-        <NavbarLink className="sm:w-auto w-full" to="/viewAttestation">View Attestation</NavbarLink>
-        <NavbarLink className="sm:w-auto w-full" to="/verifyAttestation">Verify Attestation</NavbarLink>
-      </Toolbar>
-    </div>
-  </AppBar>
+        <div
+            className={cn(
+                "flex flex-col p-4 max-w-7xl mx-auto w-full space-y-4",
+                "lg:grid lg:grid-cols-[repeat(2,auto)] lg:space-y-0 lg:space-x-4 lg:items-center"
+            )}
+        >
+            <Link href="/">
+                <h1 className={cn(
+                    "text-blue-400 text-3xl text-center",
+                    "lg:pl-4 lg:text-left",
+                    logoFont.className
+                )}>
+                    Verified
+                </h1>
+            </Link>
+            <Toolbar
+                className={cn(
+                    "flex flex-col space-y-4 justify-end",
+                    "sm:flex-row sm:space-x-4 sm:space-y-0 sm:self-center"
+                )}
+            >
+                <NavbarLink className="sm:w-auto w-full" to="/createAttestation">Create Attestation</NavbarLink>
+                <NavbarLink className="sm:w-auto w-full" to="/viewAttestation">View Attestation</NavbarLink>
+                <NavbarLink className="sm:w-auto w-full" to="/verifyAttestation">Verify Attestation</NavbarLink>
+            </Toolbar>
+        </div>
+    </AppBar>
 );
 export default NavBar;
